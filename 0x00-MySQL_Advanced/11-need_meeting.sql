3535ddfa0bc8c8e09c700c1f93d90076bb5cdd33
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 17:32:13 +0200

    Refactor need_meeting view query in 11-need_meeting.sql to use score instead of average_score for filtering

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,5 +4,5 @@ DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
     FROM students
-    WHERE average_score < 80
+    WHERE score < 80
         AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
