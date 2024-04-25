7d191eaa77388fe60381c84fd1f0e2e19abd7fca
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 17:26:06 +0200

    Add view need_meeting to filter students with average score below 80 and no recent meeting in 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates a view need_meeting
+
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE average_score < 80; AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURRENT_DATE(), INTERVAL -1 MONTH));
