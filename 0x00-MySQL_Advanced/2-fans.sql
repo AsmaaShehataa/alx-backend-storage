bd9b9d1977bc33ab09b23cbc5579d547913d0cda
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 15:32:16 +0200

    Refactor SQL script for ranking country origins of bands in metal_bands table

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,3 @@
--- script that ranks country origins of bands, ordered
--- by the number of (non-unique) fans
+-- script that ranks country origins of bands
 SELECT origin, SUM(fans) as nb_fans from metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
