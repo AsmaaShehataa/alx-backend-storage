b95c7f0e9f746180767d682deb9e76f8aeb7148b
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 16:53:25 +0200

    Refactor SQL query in 7-average_score.sql to use table alias in WHERE clause

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,14 +2,14 @@
 
 drop procedure IF EXISTS ComputeAverageScoreForUser
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
   UPDATE users
   SET average_score = (
     SELECT AVG(score)
     FROM corrections
-    WHERE corrections.userId = userId
+    WHERE corrections.user_id = user_id
   )
-  WHERE id = userId;
+  WHERE id = user_id;
 END$$
 DELIMITER ;
