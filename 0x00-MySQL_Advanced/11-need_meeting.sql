bdfc8a7067222a64d8f1ac24336d44f13859ff52
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 17:29:23 +0200

    Refactor need_meeting view query in 11-need_meeting.sql to improve readability and maintainability

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,7 @@
 
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT name FROM students
-WHERE average_score < 80; AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURRENT_DATE(), INTERVAL -1 MONTH));
+    SELECT name
+    FROM students
+    WHERE average_score < 80
+        AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURRENT_DATE(), INTERVAL -1 MONTH));
