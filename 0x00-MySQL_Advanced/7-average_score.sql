516fe1c497da402a92fc55c64bea4146e9c0ae4a
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-25 16:49:23 +0200

    Update ComputeAverageScoreForUser stored procedure to use UPDATE statement instead of INSERT statement

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,12 @@ drop procedure IF EXISTS ComputeAverageScoreForUser
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
 BEGIN
-  DECLARE average_score DECIMAL(5,2);
-  SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = userId;
-  INSERT INTO average_scores (user_id, average_score) VALUES (userId, average_score);
+  UPDATE users
+  SET average_score = (
+    SELECT AVG(score)
+    FROM corrections
+    WHERE user_id = userId
+  )
+  WHERE id = userId;
 END$$
 DELIMITER ;
