8a2d804c5caad88c202625a0b90de11acb78480c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-01 00:48:19 +0300

    Refactor ngxlog_stats function to improve code readability and fix syntax errors in 0x01-NoSQL/12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,8 +7,21 @@ Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 def ngxlog_stats(mongo_collection, option=None):
     """Function that provides some stats about Nginx logs stored in MongoDB"""
-    if option is None:
-        return mongo_collection.count_documents({})
-    if option == "method":
-        return {method: mongo_collection.count_documents({"method": method}) for method in Methods}
-    return None
+    empty_items = {}
+    if option:
+      value = mongo_collection.count_documents({
+        {"method": {"$regex": option}}})
+      print(f"\tmethod {option}: {value}")
+      return
+
+    result = mongo_collection.count_documents(empty_items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in Methods:
+        ngxlog_stats(mongo_collection, method)
+    check_status = mongo_collection.count_documents({"path": "/status"})
+    print("f{check_status} status check")
+
+if __name__ == '__main__':
+  nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+  nginx_collection(nginx_collection)
