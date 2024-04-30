636245213e9f6794da4abbfab7ae87d8112265bd
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-01 00:58:43 +0300

    Refactor update_topics function to improve code readability and use update_many in 0x01-NoSQL/10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-"""Python function that changes all topics of a school document based on the name"""
+"""Python function that changes all topics of a school"""
+
 
 def update_topics(mongo_collection, name, topics):
-    """Function that changes all topics of a school document based on the name"""
-    mongo_collection.updateMany(
-      {"name": name},
-      {"$set": {"topics": topics}}
-      )
+    """Function that changes all topics of a school"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
