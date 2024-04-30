cb1b832e318fbd47baf4d2e87ea6d26b7381af30
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-01 00:17:03 +0300

    Add script to insert a new document in the school collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Python function that inserts a new document in a collection"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Function that inserts a new document in a collection"""
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
